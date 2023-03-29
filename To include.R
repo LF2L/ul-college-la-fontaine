3263a68d40de71bbb8bd99ab1eee37bcbe8fbcc9
fabbiocrux <fabbiocrux.ms@gmail.com>
2023-03-29 08:33:18 +0200

    2023

---
+++ To include.R
@@ -0,0 +1,42 @@
+---
+   exclude: true
+# Contexte: ENSGSI - ERPI --> LF2L
+
+.pull-left[
+   
+   ```{r, out.width = '150px', fig.align='center'}
+   knitr::include_graphics(paste0(URL, 'Logos/ENSGSI/ENSGSI-small.jpg'))
+   ```
+   
+   **Ecole Nationale Supérieure en Génie des Systèmes et de l'Innovation (ENSGSI)**
+
+- École d'ingénieurs parcours en 3 ou 5 année d’études basé sur un referentiel emploi Competence spécifique pour des ingénieurs. 
+   
+   - Profil: Chefs de projets en pilotage de projets innovants. 
+   
+   
+   https://www.ensgsi.univ-lorraine.fr/
+      
+]
+
+.pull-right[
+   
+   ```{r, out.width = '280px', fig.align='center'}
+   knitr::include_graphics(paste0(URL, 'Logos/ERPI/ERPI.png'))
+   ```
+   
+   **Équipe de Recherche sur les Processus Innovatifs (ERPI)**
+      
+      - ERPI a pour vocation de mener des recherches dans le domaine de la conduite et du pilotage des processus innovatifs. 
+   
+   - L’ERPI est un laboratoire de Génie Industriel spécialisé dans l’étude des processus d’innovation. 
+   
+   https://erpi.univ-lorraine.fr/fr/
+]
+
+> **Un axe de travail de l'École ENSGSI et du laboratoire ERPI depuis 2005**: 
+- Comment évaluaer .bg-green[des processus d'Innovation] (*pas forcement l'Innovation en soi-même*)
+  - Innovation en phase amount:  Analyse de besoins,  Generation des Idées, Opportunités et validationdes concepts 
+  - Donc, les espace d'Innovation et créativité (Fablab, Tieurs Lieu) --> .bg-green.large[**Lorraine Fab Living Lab (LF2L)**]
+                                                          
+                                                          
